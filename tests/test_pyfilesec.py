178afa59be9a891cc89f461a720ccade4cc96ad9
Jeremy R. Gray <jrgray@gmail.com>
2013-08-13 13:33:14 -0400

    tests: enable py.test discovery
    - tests/ directory
    - import * gets class Tests from main file

---
+++ tests/test_pyfilesec.py
@@ -0,0 +1,7 @@
+"""Test suite for pyFileSec:
+
+This file allows py.test without args to discover and run the tests.
+The actual tests are in class Tests in the same file as the code.
+"""
+
+from pyfilesec import *
